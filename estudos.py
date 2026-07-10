d41342fa86feb5a5cd3c90c7982105acff02be58
jamilegabriely <jamile.gabriely@escolar.ifrn.edu.br>
2026-07-10 13:37:36 +0000

    Aula - 10/07

---
+++ estudos.py
@@ -0,0 +1,7 @@
+#Cancelamento: Se o parque estiver em manutenção (manutencao == "sim"), o sistema deve exibir "Parque fechado para manutenção" e mais nada.
+
+Restrição de idade/altura: Para entrar nas montanhas-russas radicais, o cliente precisa ter mais de 12 anos OU medir mais de 1.40m. Se não cumprir nenhum dos dois, exiba "Entrada recusada nas atrações radicais".
+
+Passaporte VIP: Se o cliente for VIP E a fila estiver com mais de 30 minutos, ele recebe um aviso: "Utilize a fila do FastPass!".
+
+Desconto de Estudante: Se o cliente for estudante OU menor de 21 anos, exiba: "Desconto de 50% aplicado no ingresso!".
